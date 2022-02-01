9050a7e4307bbb05d6d33249b3f99511e8497c84
ximenes <xresende@gmail.com>
2022-02-01 15:44:10 -0300

    Add setup and Makefile

--- idanalysis/__init__.py
+++ idanalysis/__init__.py
@@ -1,2 +1,6 @@
 from .deltadata import DeltaData
 from .trajectory import Trajectory
+
+import os as _os
+with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
+    __version__ = _f.read().strip()
