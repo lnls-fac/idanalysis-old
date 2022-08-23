13d8d17b13a18703406c998470ac4ef3f22632a5
Gabriel Rezende <gabriel.ascencao@lnls.br>
2022-08-23 16:26:09 -0300

    update generate_kickmap

--- scripts/wiggler/generate_kickmap.py
+++ scripts/wiggler/generate_kickmap.py
@@ -11,11 +11,11 @@ FOLDER_BASE = '/home/gabriel/repos-dev/ids-data/Wiggler/'
 def run():
     
     idkickmap = IDKickMap()
-    fmap_fname = FOLDER_BASE + "gap 059.60 mm/Fieldmap.fld"
-    posx = np.arange(-12e-3,13e-3,1e-3)
+    fmap_fname = FOLDER_BASE + "wiggler-2T-STI-main/measurement/magnetic/hallprobe/2022-08-22_Wiggler_STI_59_60mm_Fieldmap_X=-20_20mm_Z=-1650_1650mm_ID=3969.dat"
+    posx = np.arange(-20e-3,21e-3,1e-3)
     posy = np.arange(-12e-3,13e-3,1e-3)
     idkickmap.fmap_calc_kickmap(fmap_fname=fmap_fname, posx = posx, posy = posy)
-    idkickmap.generate_kickmap_file(kickmap_filename="wiggler_kickmap.txt")
+    idkickmap.generate_kickmap_file(kickmap_filename="wiggler_kickmap_new_meas.txt")
 
 if __name__ == "__main__":
    
