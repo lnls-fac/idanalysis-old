193a0ec95737efccef561401782d35718d931de5
Gabriel Rezende <gabriel.ascencao@lnls.br>
2022-08-31 17:15:23 -0300

    check termination kick

--- scripts/wiggler/check_termination_kicks.py
+++ scripts/wiggler/check_termination_kicks.py
@@ -19,8 +19,24 @@ ids = utils.create_ids()
 print(ids[0])
 
 # insert kickmap into lattice and print elements
-m = pymodels.si.create_accelerator(ids=ids)
-ind = pyaccel.lattice.find_indices(m, 'fam_name', 'WIG180')
-print(m[ind[0]])
-print(m[ind[1]])
+ring1 = pymodels.si.create_accelerator(ids=ids)
+ind = pyaccel.lattice.find_indices(ring1, 'fam_name', 'WIG180')
+idx_end = ind[-1] + 1
 
+x0 = 0
+y0 = 0
+
+coord_ini = np.array([x0, 0, y0, 0, 0, 0])
+coord_fin, *_ = pyaccel.tracking.line_pass(ring1, coord_ini, indices='open')
+spos = pyaccel.lattice.find_spos(ring1)
+
+print(coord_fin[0,idx_end]*1e3)
+print(coord_fin[1,idx_end]*1e6)
+print(coord_fin[2,idx_end]*1e3)
+print(coord_fin[3,idx_end]*1e6)
+
+print(ring1[4205].t_in)
+print(ring1[4205].t_out)
+print(ring1[4207].t_in)
+print(ring1[4207].t_out)
+print(ring1.brho**2)
