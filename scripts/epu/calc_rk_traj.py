03b8044e3e892f5ba448ea090756b08b0d316635
gabriel <gabriel.ascencao@lnls.br>
2022-10-27 11:35:33 -0300

    update calc rk script and add results

--- scripts/epu/calc_rk_traj.py
+++ scripts/epu/calc_rk_traj.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python-sirius
 
 from fieldmaptrack.common_analysis import multipoles_analysis
+from idanalysis.fmap import FieldmapOnAxisAnalysis
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -109,52 +110,90 @@ def multipolar_analysis(
     return integral_multipole_list
 
 
-def plot_rk_traj(idconfig, traj_init_rx, traj_init_ry, rk_s_step=0.2):
+def plot_rk_traj(
+        idconfig, gap, phase, traj_init_rx, traj_init_ry, rk_s_step=0.2):
     """."""
     # create IDKickMap
     idkickmap = create_idkickmap(idconfig)
-
     idkickmap.rk_s_step = rk_s_step
     idkickmap.fmap_calc_trajectory(
         traj_init_rx=traj_init_rx, traj_init_ry=traj_init_ry)
-    traj = idkickmap.traj
 
+    traj = idkickmap.traj
     fmap = idkickmap.fmap_config.fmap
     rz = fmap.rz
     bx = fmap.bx[fmap.ry_zero][fmap.rx_zero][:]
     by = fmap.by[fmap.ry_zero][fmap.rx_zero][:]
     bz = fmap.bz[fmap.ry_zero][fmap.rx_zero][:]
+
+    rz = traj.rz
+    bx = traj.bx
+    by = traj.by
+
+    fieldtools = FieldmapOnAxisAnalysis()
+    ibx = fieldtools.calc_first_integral(bx, rz)
+    iby = fieldtools.calc_first_integral(by, rz)
+    iibx = fieldtools.calc_second_integral(ibx, rz)
+    iiby = fieldtools.calc_second_integral(iby, rz)
+
     plt.figure(1)
     plt.plot(rz, bx, color='b', label="Bx")
     plt.plot(rz, by, color='C1', label="By")
-    plt.plot(rz, bz, color='g', label="Bz")
+    # plt.plot(rz, bz, color='g', label="Bz")
     plt.xlabel('rz [mm]')
     plt.ylabel('Field [T]')
     plt.grid()
     plt.legend()
+    plt.title(
+        'Field components for gap {} mm and phase {} mm'.format(gap, phase))
+    fig_path = 'results/gap ' + gap + '/' + idconfig + '/Field.png'
+    plt.savefig(fig_path, dpi=300)
+    plt.close()
 
     labelx = 'rx @ end: {:+.1f} um'.format(1e3*traj.rx[-1])
     labely = 'ry @ end: {:+.1f} um'.format(1e3*traj.ry[-1])
     plt.figure(2)
-    plt.plot(traj.rz, 1e3*traj.rx, '.-', label=labelx)
-    plt.plot(traj.rz, 1e3*traj.ry, '.-', label=labely)
+    plt.plot(traj.rz, 1e3*traj.rx, '-', color='b', label=labelx)
+    plt.plot(traj.rz, 1e3*traj.ry, '-', color='C1', label=labely)
     plt.xlabel('rz [mm]')
     plt.ylabel('pos [um]')
-    plt.legend()
     plt.grid()
-    plt.title('Runge-Kutta Trajectory Pos ({})'.format(idconfig))
-    plt.show()
+    plt.legend()
+    plt.title(
+        'RK Trajectory Pos for gap {} mm and phase {} mm'.format(gap, phase))
+    fig_path = 'results/gap ' + gap + '/' + idconfig + '/RK Pos.png'
+    plt.savefig(fig_path, dpi=300)
+    plt.close()
 
     labelx = 'px @ end: {:+.1f} urad'.format(1e6*traj.px[-1])
     labely = 'py @ end: {:+.1f} urad'.format(1e6*traj.py[-1])
-    plt.plot(traj.rz, 1e6*traj.px, '.-', label=labelx)
-    plt.plot(traj.rz, 1e6*traj.py, '.-', label=labely)
+    plt.figure(3)
+    plt.plot(traj.rz, 1e6*traj.px, '-', color='b', label=labelx)
+    plt.plot(traj.rz, 1e6*traj.py, '-', color='C1', label=labely)
     plt.xlabel('rz [mm]')
     plt.ylabel('ang [urad]')
     plt.legend()
     plt.grid()
-    plt.title('Runge-Kutta Trajectory Ang ({})'.format(idconfig))
-    plt.show()
+    plt.title(
+        'RK Trajectory Ang for gap {} mm and phase {} mm'.format(gap, phase))
+    fig_path = 'results/gap ' + gap + '/' + idconfig + '/RK Ang.png'
+    plt.savefig(fig_path, dpi=300)
+    plt.close()
+
+    print('Field Integrals and traj for gap {} mm and phase {} mm'.format(
+        gap, phase))
+    print('IBx {:.3e} Tm'.format(ibx[-1]))
+    print('px {:+.2f} urad'.format(1e6*traj.py[-1]))
+    print()
+    print('IIBx {:.3e} Tm²'.format(iibx[-1]))
+    print('rx {:+.2f} um'.format(1e3*traj.ry[-1]))
+    print()
+    print('IBy {:.3e} Tm'.format(iby[-1]))
+    print('py {:+.2f} urad'.format(1e6*traj.px[-1]))
+    print()
+    print('IIBy {:.3e} Tm²'.format(iiby[-1]))
+    print('ry {:+.2f} um'.format(1e3*traj.rx[-1]))
+    print()
 
 
 def run_multipole_analysis():
@@ -247,6 +286,28 @@ def run_multipole_analysis():
     plt.close()
 
 
+def run_traj_analysis():
+    """."""
+    traj_init_rx = 0.0  # [mm]
+    traj_init_ry = 0.0  # [mm]
+    rk_s_step = 0.2  # [mm]
+
+    gap22 = ['ID4083', 'ID4081', 'ID4079', 'ID4080', 'ID4082']  # gap 22.0
+    gap23 = ['ID4103', 'ID4101', 'ID4099', 'ID4100', 'ID4102']  # gap 23.3
+    gap25 = ['ID4088', 'ID4086', 'ID4084', 'ID4085', 'ID4087']  # gap 25.7
+    gap29 = ['ID4093', 'ID4091', 'ID4089', 'ID4090', 'ID4092']  # gap 29.3
+    gap40 = ['ID4098', 'ID4096', 'ID4094', 'ID4095', 'ID4097']  # gap 40.9
+    gap_list = [gap22, gap23, gap25, gap29, gap40]
+    phases = ['-25', '-16.39', '0', '16.39', '25']
+    gaps_values = ['22.0', '23.3', '25.7', '29.3', '40.9']
+    for i, idgap in enumerate(gap_list):
+        gap = gaps_values[i]
+        for j, config in enumerate(idgap):
+            phase = phases[j]
+            plot_rk_traj(
+                config, gap, phase,  traj_init_rx, traj_init_ry, rk_s_step=0.2)
+
+
 if __name__ == "__main__":
     """."""
-    run_multipole_analysis()
+    run_traj_analysis()
